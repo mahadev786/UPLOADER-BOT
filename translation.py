6e6323ff4706924ac1c7b8da2cb199b96e1698f5
mahadev786 <68583640+mahadev786@users.noreply.github.com>
2022-03-09 11:10:31 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,17 +1,17 @@
 class Translation(object):
     START_TEXT = """Hi {},
-I'm URL X Uploader!
+I'm 𝘿𝙚𝙫 𝙐𝙍𝙇 𝙐𝙋𝙇𝙊𝘼𝘿𝙀𝙍 𝘽𝙊𝙏!
 You can upload HTTP/HTTPS direct link, Using this bot!
 
 /help for more details!"""
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
-    DOWNLOAD_START = "📥Downloading..."
-    UPLOAD_START = "📤Uploading..."
+    DOWNLOAD_START = "📥𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙞𝙣𝙜..."
+    UPLOAD_START = "📤𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜..."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @Uploader_X_bot\n\n<b>Join : @Space_X_Bots</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@Uploader_X_Bot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @Dev_uploadbot\n\n<b>Join : @Deva_tg_update</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@Dev_uploadbot"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     CUSTOM_CAPTION_UL_FILE = "{}"
@@ -26,7 +26,7 @@ URL | filename | username | password"""
    Video  - Give File as video without Screenshots
    File   - Give File without Screenshots
 
-If bot didn't respond, contact @Sources_Codes"""
+If bot didn't respond, contact @Dev_tg_update"""
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
 You can use /rename command after receiving file to rename it with custom thumbnail support.
